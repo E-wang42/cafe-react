6b13f05af52991bed714bc53c410513c98d586d2
eric <ericwang42@outlook.com>
2022-11-17 21:19:23 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button class="btn btn-primary">Button</button>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/About">ABOUT</Link>
+          </li>
+          <li>
+            <Link to="/Menu">MENU</Link>
+          </li>
+          <li>
+            <Link to="/Contact">CONTACT</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      {/* <div className="App">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button class="btn btn-primary">Button</button>
+      </div> */}
+    </>
   );
 }
 
