f95b00f9941f791775a06f96cdb52448c30f5eed
eric <ericwang42@outlook.com>
2022-11-18 23:03:08 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,10 @@ import NotFound from "./pages/404";
 
 function App() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">HOME</NavLink>
-          </li>
-          <li>
-            <NavLink to="/About">ABOUT</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Menu">MENU</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Contact">CONTACT</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-
-      {/* <div className="App">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <button class="btn btn-primary">Button</button>
-      </div> */}
-    </>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Button</button>
+    </div>
   );
 }
 
