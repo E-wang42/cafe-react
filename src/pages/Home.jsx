3a188fe064d2f0bb7908cd5a6753fe921b84237c
eric <ericwang42@outlook.com>
2022-11-21 22:56:11 -0800

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="h-screen">
+      <div className="h-full grid place-items-center">
+        <motion.div className="w-8 h-8 bg-white"></motion.div>
+        <h1 id="heading">Panda Coffee House</h1>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
