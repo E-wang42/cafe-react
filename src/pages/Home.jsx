a51495782c12d88668eda7a748d56fb7532030b6
eric <ericwang42@outlook.com>
2022-11-23 15:03:47 -0800

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 function Home() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
-    <section className="h-screen">
-      <div className="h-full grid place-items-center">
-        <motion.div className="w-8 h-8 bg-white"></motion.div>
-        <h1 id="heading">Panda Coffee House</h1>
-      </div>
-    </section>
+    <>
+      <section id="section2" className="h-screen border-red-700" ref={ref}>
+        <h1
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+          className="text-6xl font-serif text-dark-coffee"
+          id="heading"
+        >
+          Panda Coffee House
+        </h1>
+      </section>
+    </>
   );
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "light-coffee": "#be4632",
+      "dark-coffee": "#1f1919",
+      "light-grey": "#e6e6e6",
+      "dark-grey": "#625d5d",
+    },
+    extend: {
+      fontFamily: {
+        cormorant: [`"Cormorant Upright"`, "serif"],
+        fira: [`"Fira Sans"`, "serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
