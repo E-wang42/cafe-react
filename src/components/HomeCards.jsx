7d2aa8d4160bfa7a1af555e148155b2089e24e7f
eric <ericwang42@outlook.com>
2022-11-24 23:55:16 -0800

    update

---
+++ src/components/HomeCards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeCards() {
+  return <div>HomeCards</div>;
+}
+
+export default HomeCards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
+import "../assets/hero1.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 
 function Home() {
   const ref = useRef(null);
@@ -8,8 +14,29 @@ function Home() {
 
   return (
     <>
-      <section id="section2" className="h-screen border-red-700" ref={ref}>
-        <h1
+      <section id="section2" className="h-screen">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+        {/* <h1
           style={{
             transform: isInView ? "none" : "translateX(-200px)",
             opacity: isInView ? 1 : 0,
@@ -19,7 +46,7 @@ function Home() {
           id="heading"
         >
           Panda Coffee House
-        </h1>
+        </h1> */}
       </section>
     </>
   );
